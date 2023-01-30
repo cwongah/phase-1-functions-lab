8b8005fe586f240075c4b7423a96aff73408d569
Christopher Wong <cw30355@gmail.com>
2023-01-30 15:37:40 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    return Math.abs(block - 42)
+}
+
+function distanceFromHqInFeet(block){
+    return distanceFromHqInBlocks(block) * 264
+}
+
+function distanceTravelledInFeet(block1, block2){
+    return Math.abs(block1 - block2) * 264
+}
+
+function calculatesFarePrice(block1, block2){
+    let distance = distanceTravelledInFeet(block1, block2)
+    let farePrice
+    if(distance <= 400){
+        farePrice = 0
+    }
+    if(distance > 400 && distance <= 2000){
+        farePrice = 0.02 * (distance - 400)
+    }
+    if(distance > 2000 && distance <=2500){
+        farePrice = 25
+    }
+    if(distance > 2500){
+        farePrice = "cannot travel that far"
+    }
+    return farePrice
+}
